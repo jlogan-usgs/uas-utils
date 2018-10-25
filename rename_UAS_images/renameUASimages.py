5ec5313ecccb18505e4d60adadcea92c96b8980a
Joshua Logan <jlogan@usgs.gov>
2018-10-25 16:02:15 -0700

    replaced PIL with exifread, now works for JPG and DNG

--- rename_UAS_images/renameUASimages.py
+++ rename_UAS_images/renameUASimages.py
@@ -9,68 +9,26 @@ import argparse
 from pathlib import Path
 from tqdm import tqdm
 from datetime import datetime, timedelta
-from PIL import Image
-from PIL.ExifTags import TAGS
+import exifread
 
 #file types to process
 ftypes = ['JPG', 'DNG']
 
-def get_exif(fn):
-    ret = {}
-    i = Image.open(fn)
-    info = i._getexif()
-    for tag, value in info.items():
-        decoded = TAGS.get(tag, tag)
-        ret[decoded] = value
-    return ret
-
-# ===========================  BEGIN PARSER ==============================
-descriptionstr = ('  Script to rename images collected with UAS camera.')
-
-parser = argparse.ArgumentParser(description=descriptionstr, 
-                                 epilog='example: run renameUASimages.py '
-                                         '-dir=D:/mydir/data/images -f=2 '
-                                         '-utcoffset=-8 '
-                                         '-sepdir')
-#input directory arg
-parser.add_argument('-dir', '--input_directory', dest='indir',  
-                    required=True,
-                    help='input directory with images')
-
-#flight arg
-parser.add_argument('-f', '--flight_number', dest='fltnum',  
-                    type=int, required=True,
-                    help='flight number')
-
-#utc offset arg
-parser.add_argument('-utc', '--utc_offset', dest='utcoffset',  
-                    type=int, required=True,
-                    help='image tzone to utc offset in hours [example: PST to UTC = 8]')
-
-#arg to separate files into raw and jpg dir
-parser.add_argument('-sepdir', '--sep_files_dir', help='separate raw and jpg into separate dir',
-    action='store_true')
-
-#parse
-args = parser.parse_args()
-
-##check required args
-#if args.utcoffset is None:
-#    raise ValueError('utcoffset must be provided')
-#    
-#if args.fltnum is None:
-#    raise ValueError('flight number must be provided')
-
-#set var
-fltnum = args.fltnum
-indir = Path(args.indir)
-sepdir = args.sep_files_dir
-utcoffset = args.utcoffset
+#using exifread which works for DNG and JPG
+def get_dt_orignal(fn):
+    '''Gets DateTimeOriginal value from EXIF'''
+    with open(fn, 'rb') as i:
+        tags = exifread.process_file(i)
+    dt_orig = str(tags.get('EXIF DateTimeOriginal'))
+    return dt_orig
 
 def new_image_name(img, utcoffset, fltnum):
     '''Formats new image name.'''
     #get image time stamp
-    imgdt = datetime.strptime(Image.open(img)._getexif()[36867], '%Y:%m:%d %H:%M:%S')
+    #PIL doesn't work for DNG
+    #imgdt = datetime.strptime(Image.open(img)._getexif()[36867], '%Y:%m:%d %H:%M:%S')  
+    #using exifread instead
+    imgdt = datetime.strptime(get_dt_orignal(img), '%Y:%m:%d %H:%M:%S')
     #add UTC offset to get UTC time
     imgutcdt = imgdt + timedelta(hours=utcoffset)
     #format output utc dt
@@ -93,11 +51,42 @@ def user_prompt(question:str):
         except ValueError:
             sys.stdout.write("Please use y/n or yes/no.\n")
 
+# ===========================  BEGIN PARSER ==============================
+descriptionstr = ('  Script to rename images collected with UAS camera.')
+parser = argparse.ArgumentParser(description=descriptionstr, 
+                                 epilog='example: run renameUASimages.py '
+                                         '-dir=D:/mydir/data/images -f=2 '
+                                         '-utcoffset=-8 '
+                                         '-sepdir')
+#input directory arg
+parser.add_argument('-dir', '--input_directory', dest='indir',  
+                    required=True,
+                    help='input directory with images')
+#flight arg
+parser.add_argument('-f', '--flight_number', dest='fltnum',  
+                    type=int, required=True,
+                    help='flight number')
+#utc offset arg
+parser.add_argument('-utc', '--utc_offset', dest='utcoffset',  
+                    type=int, required=True,
+                    help='image tzone to utc offset in hours [example: PST to UTC = 8]')
+#arg to separate files into raw and jpg dir
+parser.add_argument('-sepdir', '--sep_files_dir', help='separate raw and jpg into separate dir',
+    action='store_true')
+#parse
+args = parser.parse_args()
+
+#set var
+fltnum = args.fltnum
+indir = Path(args.indir)
+sepdir = args.sep_files_dir
+utcoffset = args.utcoffset
+
 #Show sample rename to user
 ftype = ftypes[0]
 fsample = next(indir.glob('*.' + ftype))
 print('Image ' + fsample.name + ' has time stamp: ' + 
-      str(datetime.strptime(Image.open(fsample)._getexif()[36867], '%Y:%m:%d %H:%M:%S')) +
+      str(datetime.strptime(get_dt_orignal(fsample), '%Y:%m:%d %H:%M:%S')) +
       '\n' + 'Image ' + fsample.name + ' will be renamed to: ' + 
       new_image_name(fsample, utcoffset, fltnum)+ '\n')
 
